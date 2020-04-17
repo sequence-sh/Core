0d4a0d23c576a23a017ae31710f0a7ca2d877e81
Mark Wainwright <wainwrightml@googlemail.com>
2020-04-17 10:37:07 +0100

    Added property version attribute

---
+++ Processes/RequiredVersionAttribute.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Reductech.EDR.Utilities.Processes
+{
+    /// <summary>
+    /// Use this attribute to denote the required version of some software.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property)]
+    public sealed class RequiredVersionAttribute : Attribute
+    {
+        /// <summary>
+        /// Create a new RequiredVersion attribute
+        /// </summary>
+        /// <param name="softwareName">e.g. "Nuix"</param>
+        /// <param name="requiredVersion">e.g. "6.2"</param>
+        public RequiredVersionAttribute(string softwareName, string requiredVersion)
+        {
+            SoftwareName = softwareName;
+            RequiredVersion = new Version(requiredVersion);
+        }
+
+        /// <summary>
+        /// The software whose version is required.
+        /// </summary>
+        public string SoftwareName { get; }
+
+        /// <summary>
+        /// The required version of the software.
+        /// </summary>
+        public Version RequiredVersion { get; }
+
+    }
+}
