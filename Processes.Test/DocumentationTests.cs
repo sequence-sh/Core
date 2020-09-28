bb37e6edc29ce8dd463f380de25ce4d925842508
Mark Wainwright <wainwrightml@gmail.com>
2020-09-28 09:21:32 +0100

    Enabled mutation testing

---
+++ Processes.Test/DocumentationTests.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using Reductech.EDR.Processes.General;
+using Reductech.EDR.Processes.Internal;
+using Reductech.EDR.Processes.Internal.Documentation;
+using Reductech.EDR.Processes.Test.Extensions;
+using Xunit;
+using Xunit.Abstractions;
+using ITestCase = Reductech.EDR.Processes.Test.Extensions.ITestCase;
+
+namespace Reductech.EDR.Processes.Test
+{
+    public class DocumentationTests : DocumentationTestCases
+    {
+        public DocumentationTests(ITestOutputHelper testOutputHelper) => TestOutputHelper = testOutputHelper;
+
+
+        /// <inheritdoc />
+        [Theory]
+        [ClassData(typeof(DocumentationTestCases))]
+        public override void Test(string key) => base.Test(key);
+    }
+
+    public class DocumentationTestCases : TestBase
+    {
+        /// <inheritdoc />
+        protected override IEnumerable<ITestCase> TestCases
+        {
+            get
+            {
+                yield return new DocumentationTestCase(@"# Processes
+<a name=""Not""></a>
+## Not
+
+**Boolean**
+
+Negation of a boolean value.
+
+|Parameter|Type  |Required|Summary             |
+|:-------:|:----:|:------:|:------------------:|
+|Boolean  |`bool`|☑️      |The value to negate.|", NotProcessFactory.Instance);
+            }
+        }
+
+
+
+        private class DocumentationTestCase : ITestCase
+        {
+            /// <summary>
+            /// Create a new DocumentationTestCase
+            /// </summary>
+            public DocumentationTestCase(string text,
+                params IRunnableProcessFactory[] entities)
+            {
+                Text = text;
+                Entities = entities;
+                Name = string.Join(" ", Entities.Select(x => x.TypeName).OrderBy(x=>x));
+            }
+
+            /// <inheritdoc />
+            public string Name { get; }
+
+            public string Text { get; }
+
+            public IReadOnlyCollection<IRunnableProcessFactory> Entities { get; }
+
+            /// <inheritdoc />
+            public void Execute(ITestOutputHelper testOutputHelper)
+            {
+                var documented = Entities.Select(x => new ProcessWrapper(x, new DocumentationCategory("Processes")));
+
+                var lines = DocumentationCreator.CreateDocumentationLines(documented);
+
+                foreach (var line in lines)
+                {
+                    testOutputHelper.WriteLine(line);
+                }
+
+                var textLines = string.Join("\r\n", lines);
+
+                textLines.Trim().Should().Be(Text.Trim());
+
+            }
+        }
+    }
+
+}
--- Processes/Internal/Documentation/IParameter.cs
+++ Processes/Internal/Documentation/IParameter.cs
@@ -1,58 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace Reductech.EDR.Processes.Internal.Documentation
 {
-    using System;
-    using System.Collections.Generic;
-
-    namespace Reductech.Utilities.InstantConsole
+    /// <summary>
+    /// The parameter to a runnable method.
+    /// </summary>
+    public interface IParameter
     {
         /// <summary>
-        /// The parameter to a runnable method.
+        /// The name of the parameter.
         /// </summary>
-        public interface IParameter
-        {
-            /// <summary>
-            /// The name of the parameter.
-            /// </summary>
-            string Name { get; }
+        string Name { get; }
 
-            /// <summary>
-            /// /// A summary of what this parameter does.
-            /// </summary>
-            string Summary { get; }
+        /// <summary>
+        /// /// A summary of what this parameter does.
+        /// </summary>
+        string Summary { get; }
 
-            /// <summary>
-            /// The type of the parameter.
-            /// </summary>
-            Type Type { get; }
+        /// <summary>
+        /// The type of the parameter.
+        /// </summary>
+        Type Type { get; }
 
-            /// <summary>
-            /// Is this parameter required.
-            /// </summary>
-            bool Required { get; }
+        /// <summary>
+        /// Is this parameter required.
+        /// </summary>
+        bool Required { get; }
 
-            /// <summary>
-            /// Extra fields e.g. Examples, Default Values, Requirements
-            /// </summary>
-            IReadOnlyDictionary<string, string> ExtraFields { get; }
+        /// <summary>
+        /// Extra fields e.g. Examples, Default Values, Requirements
+        /// </summary>
+        IReadOnlyDictionary<string, string> ExtraFields { get; }
 
-            ///// <summary>
-            ///// An example of a valid value for this parameter.
-            ///// </summary>
-            //string? Example { get; }
+        ///// <summary>
+        ///// An example of a valid value for this parameter.
+        ///// </summary>
+        //string? Example { get; }
 
-            ///// <summary>
-            ///// The default value of this parameter, as a human readable string, if applicable.
-            ///// </summary>
-            //string? DefaultValueString { get; }
+        ///// <summary>
+        ///// The default value of this parameter, as a human readable string, if applicable.
+        ///// </summary>
+        //string? DefaultValueString { get; }
 
-            ///// <summary>
-            ///// Requirements for using this parameter.
-            ///// </summary>
-            //string? Requirements { get; }
-        }
+        ///// <summary>
+        ///// Requirements for using this parameter.
+        ///// </summary>
+        //string? Requirements { get; }
     }
 }
