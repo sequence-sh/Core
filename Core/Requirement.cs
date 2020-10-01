a5d8c6901f30355c0635839edb7a36c49f86edf1
Mark Wainwright <wainwrightml@gmail.com>
2020-10-01 11:58:45 +0100

    Removed some unused methods

--- Core/Requirement.cs
+++ Core/Requirement.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
 using System.Text;
-using CSharpFunctionalExtensions;
-using Reductech.EDR.Core.Util;
 using YamlDotNet.Serialization;
 
 namespace Reductech.EDR.Core
@@ -40,96 +36,96 @@ namespace Reductech.EDR.Core
         /// </summary>
         public string? Notes { get; set; }
 
-        /// <summary>
-        /// Tries to convert an object to a requirement
-        /// </summary>
-        public static Result<Requirement> TryConvert(object o)
-        {
-            if (o is Requirement req) return req;
+        ///// <summary>
+        ///// Tries to convert an object to a requirement
+        ///// </summary>
+        //public static Result<Requirement> TryConvert(object o)
+        //{
+        //    if (o is Requirement req) return req;
 
-            if (o is IReadOnlyDictionary<object, object> dictionary)
-            {
-                var requirement = new Requirement();
+        //    if (o is IReadOnlyDictionary<object, object> dictionary)
+        //    {
+        //        var requirement = new Requirement();
 
-                var results = new List<Result>();
-
-
-                foreach (var (key, value) in dictionary.Where(x => x.Value != null))
-                {
-                    var result = key switch
-                    {
-                        nameof(MinVersion) => TryConvertToVersion(value).Tap(x => requirement.MinVersion = x),
-                        nameof(MaxVersion) => TryConvertToVersion(value).Tap(x => requirement.MaxVersion = x),
-                        nameof(Name) => value.TryConvert<string>().Tap(x => requirement.Name = x),
-                        nameof(Notes) => value.TryConvert<string>().Tap(x => requirement.Notes = x),
-                        _ => Result.Failure($"Could not recognize property '{key}'.")
-                    };
-
-                    results.Add(result);
-                }
-
-                var r = results
-                    .Combine().Bind<Requirement>(() => requirement);
-
-                return r;
-
-            }
+        //        var results = new List<Result>();
 
 
-            return Result.Failure<Requirement>($"Could not convert '{o}' to {nameof(Requirement)}");
-        }
+        //        foreach (var (key, value) in dictionary.Where(x => x.Value != null))
+        //        {
+        //            var result = key switch
+        //            {
+        //                nameof(MinVersion) => TryConvertToVersion(value).Tap(x => requirement.MinVersion = x),
+        //                nameof(MaxVersion) => TryConvertToVersion(value).Tap(x => requirement.MaxVersion = x),
+        //                nameof(Name) => value.TryConvert<string>().Tap(x => requirement.Name = x),
+        //                nameof(Notes) => value.TryConvert<string>().Tap(x => requirement.Notes = x),
+        //                _ => Result.Failure($"Could not recognize property '{key}'.")
+        //            };
 
-        private static Result<Version> TryConvertToVersion(object o)
-        {
-            if (o is Version ver) return ver;
+        //            results.Add(result);
+        //        }
 
-            if (o is IReadOnlyDictionary<object, object> dictionary)
-            {
-                var major = -1;
-                var minor = -1;
-                var build = -1;
-                var revision = -1;
+        //        var r = results
+        //            .Combine().Bind<Requirement>(() => requirement);
 
-                var results = new List<Result>();
+        //        return r;
 
-
-                foreach (var (key, value) in dictionary.Where(x => x.Value != null))
-                {
-                    var result = key switch
-                    {
-                        nameof(Version.Major) => value.TryConvert<int>().Tap(x =>major = x),
-                        nameof(Version.Minor) => value.TryConvert<int>().Tap(x =>minor = x),
-                        nameof(Version.Build) => value.TryConvert<int>().Tap(x =>build = x),
-                        nameof(Version.Revision) => value.TryConvert<int>().Tap(x =>revision = x),
-                        nameof(Version.MajorRevision) => Result.Success(),
-                        nameof(Version.MinorRevision) => Result.Success(),
-                        _ => Result.Failure($"Could not recognize property '{key}'.")
-                    };
-
-                    results.Add(result);
-                }
-
-                var r = results
-                    .Combine().Bind(() =>
-                    {
-                        Version version;
-
-                        if (major < 0 || minor < 0)
-                            return Result.Failure<Version>("Major and Minor versions must both be greater than zero");
-                        if (build < 0) version = new Version(major, minor);
-                        else if (revision < 0) version = new Version(major, minor, build);
-                        else version = new Version(major, minor, build, revision);
-
-                        return version;
-                    });
-
-                return r;
-
-            }
+        //    }
 
 
-            return Result.Failure<Version>($"Could not convert '{o}' to {nameof(Version)}");
-        }
+        //    return Result.Failure<Requirement>($"Could not convert '{o}' to {nameof(Requirement)}");
+        //}
+
+        //private static Result<Version> TryConvertToVersion(object o)
+        //{
+        //    if (o is Version ver) return ver;
+
+        //    if (o is IReadOnlyDictionary<object, object> dictionary)
+        //    {
+        //        var major = -1;
+        //        var minor = -1;
+        //        var build = -1;
+        //        var revision = -1;
+
+        //        var results = new List<Result>();
+
+
+        //        foreach (var (key, value) in dictionary.Where(x => x.Value != null))
+        //        {
+        //            var result = key switch
+        //            {
+        //                nameof(Version.Major) => value.TryConvert<int>().Tap(x =>major = x),
+        //                nameof(Version.Minor) => value.TryConvert<int>().Tap(x =>minor = x),
+        //                nameof(Version.Build) => value.TryConvert<int>().Tap(x =>build = x),
+        //                nameof(Version.Revision) => value.TryConvert<int>().Tap(x =>revision = x),
+        //                nameof(Version.MajorRevision) => Result.Success(),
+        //                nameof(Version.MinorRevision) => Result.Success(),
+        //                _ => Result.Failure($"Could not recognize property '{key}'.")
+        //            };
+
+        //            results.Add(result);
+        //        }
+
+        //        var r = results
+        //            .Combine().Bind(() =>
+        //            {
+        //                Version version;
+
+        //                if (major < 0 || minor < 0)
+        //                    return Result.Failure<Version>("Major and Minor versions must both be greater than zero");
+        //                if (build < 0) version = new Version(major, minor);
+        //                else if (revision < 0) version = new Version(major, minor, build);
+        //                else version = new Version(major, minor, build, revision);
+
+        //                return version;
+        //            });
+
+        //        return r;
+
+        //    }
+
+
+        //    return Result.Failure<Version>($"Could not convert '{o}' to {nameof(Version)}");
+        //}
 
         /// <inheritdoc />
         public override string ToString()
