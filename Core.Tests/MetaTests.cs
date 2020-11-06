876cf48a415854713829bbf9db88af8a2647e11e
Mark Wainwright <wainwrightml@gmail.com>
2020-11-06 09:50:32 +0000

    Removed some commented out code

--- Core.Tests/MetaTests.cs
+++ Core.Tests/MetaTests.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Linq;
-using System.Reflection;
-using FluentAssertions;
+﻿using System.Reflection;
 using Reductech.EDR.Core.Internal;
 using Reductech.EDR.Core.TestHarness;
-using Xunit;
 
 namespace Reductech.EDR.Core.Tests
 {
