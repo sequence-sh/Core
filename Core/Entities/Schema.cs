449b63a26e506c3a752f329f919d2c9553303e98
Mark Wainwright <wainwrightml@gmail.com>
2020-11-06 12:11:46 +0000

    Created Schema and some enums

---
+++ Core/Entities/Schema.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Reductech.EDR.Core.Entities
+{
+    /// <summary>
+    /// An entity schema.
+    /// Enforces that the entity matches certain constraints.
+    /// </summary>
+    public sealed class Schema
+    {
+    }
+
+    public enum Multiplicity
+    {
+        Any,
+        AtLeastOne,
+        ExactlyOne,
+        UpToOne
+    }
+
+    public enum PropertySchemaType
+    {
+        String,
+        Integer,
+        Double,
+        Enum,
+        Bool,
+        Date
+
+    }
+}
