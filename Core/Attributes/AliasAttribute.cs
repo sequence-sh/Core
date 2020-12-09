9de384c8a07b6ffa47497dfc4a12b911b052cd28
Mark Wainwright <wainwrightml@gmail.com>
2020-12-09 13:51:22 +0000

    Allowed property aliases

---
+++ Core/Attributes/AliasAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Reductech.EDR.Core.Attributes
+{
+    /// <summary>
+    /// Allows you to define an alternative name for a property
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = true)]
+    public sealed class AliasAttribute : Attribute
+    {
+        /// <summary>
+        /// Create a new AliasAttribute
+        /// </summary>
+        public AliasAttribute(string name) => Name = name;
+
+        /// <summary>
+        /// The alternative name for the property
+        /// </summary>
+        public string Name { get; }
+    }
+}
