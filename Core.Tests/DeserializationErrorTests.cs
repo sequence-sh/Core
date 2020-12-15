f0f2161e07f954773b82a390c20355becde46ca5
Mark Wainwright <wainwrightml@gmail.com>
2020-12-15 15:38:47 +0000

    Reenabled Deserialization error cases

--- Core.Tests/DeserializationErrorTests.cs
+++ Core.Tests/DeserializationErrorTests.cs
@@ -19,7 +19,7 @@ namespace Reductech.EDR.Core.Tests
 
 
         /// <inheritdoc />
-        [Theory(Skip = "true")]
+        [Theory()]
         [ClassData(typeof(DeserializationErrorTestCases))]
         public override void Test(string key) => base.Test(key);
     }
@@ -31,68 +31,31 @@ namespace Reductech.EDR.Core.Tests
         {
             get
             {
+                yield return new DeserializationErrorCase("", ("Sequence is empty.", EntireSequenceLocation.Instance.AsString));
 
-                yield return new DeserializationErrorCase("- <Entity> = 123\n- Print(Value = <Entity>)",
-                    ("The Variable <Entity> is Reserved.", "<Entity> = 123")
+                yield return new DeserializationErrorCase("- <Entity> = 123\n- Print <Entity>",
+                    ("The Variable <Entity> is Reserved.", "<Entity> = 123"));
+
+                yield return new DeserializationErrorCase("\"Print 123\"", ("Yaml must represent a step with return type Unit", "\"Print 123\""));
+
+                yield return new DeserializationErrorCase("'Print 123'", ("Yaml must represent a step with return type Unit", "'Print 123'"));
+
+
+                yield return new DeserializationErrorCase("Print Value: Hello Value: World",
+                    ("Duplicate Parameter 'Word'", "Line: 1, Col: 1, Idx: 0 - Line: 4, Col: 5, Idx: 38")
                     );
 
+                yield return new DeserializationErrorCase("Print Value: 'hello' Term: 'world')",
+                ("Unexpected Parameter 'Term' in 'Print'", "Line: 1, Col: 1, Idx: 0 - Line: 1, Col: 38, Idx: 37"));
 
-                yield return new DeserializationErrorCase("- <ReductechEntity> = 123\n- Print(Value = <ReductechEntity>)",
-                    ("The Variable Prefix 'Reductech' is Reserved.", "<ReductechEntity> = 123")
-                    );
+                yield return new DeserializationErrorCase("Print(['abc', '123'] == ['abc', '123'])",
+                    ("Cannot compare objects of type 'ListOfString'", "['abc', '123'] == ['abc', '123']"));
 
-                yield return new DeserializationErrorCase("", ("Yaml is empty.", EntireSequenceLocation.Instance.AsString));
+                yield return new DeserializationErrorCase("MyMegaFunction true", ("The step 'MyMegaFunction' does not exist", "MyMegaFunction true"));
 
-                yield return new DeserializationErrorCase("\"Print(Value = 123)\"", ("Yaml must represent a step with return type Unit", "Print(Value = 123)"));
-
-                yield return new DeserializationErrorCase("'Print(Value = 123)'", ("Yaml must represent a step with return type Unit", "Print(Value = 123)"));
-
-                yield return new DeserializationErrorCase("Do:Nothing",("unexpected `D`", "Line: 1, Col: 1, Idx: 0 - Line: 1, Col: 11, Idx: 10"));
-
-                yield return new DeserializationErrorCase("Do: Print\nValue:Hello",
-                    ("While scanning a simple key, could not find expected ':'.", "Line: 2, Col: 1, Idx: 10 - Line: 2, Col: 1, Idx: 10"));
-
-
-                yield return new DeserializationErrorCase("Do: Print\nWord: Hello\nWord: World\nText: Goodbye",
-                    ("Duplicate Parameter 'Word'", "Line: 1, Col: 1, Idx: 0 - Line: 4, Col: 5, Idx: 38")//, ("Missing Parameter 'Value' in 'Print'", "Line: 1, Col: 1, Idx: 0 - Line: 5, Col: 1, Idx: 47")
-                    );
-
-                yield return new DeserializationErrorCase("Do: Print\nDo: Print\nValue: Hello",
-                    ("Unexpected Parameter 'Do' in 'Print'", "Line: 1, Col: 1, Idx: 0 - Line: 4, Col: 1, Idx: 32")
-                );
-                yield return new DeserializationErrorCase("Do: Print\nValue: Hello\nConfig:\n DoNotSplit: false\nConfig:\n DoNotSplit: true",
-                    ("Duplicate Parameter 'Config'", "Line: 1, Col: 1, Idx: 0 - Line: 7, Col: 1, Idx: 75")
-                    );
-
-
-                yield return new DeserializationErrorCase("Da: Print\nValue: Hello",
-                    ("Could not deserialize an object whose first property is 'Da'", "Line: 1, Col: 1, Idx: 0 - Line: 1, Col: 10, Idx: 9")
-                );
-
-                yield return new DeserializationErrorCase("Print(Word = 'hello', Term = 'world')",
-                    ("Unexpected Parameter 'Word' in 'Print'", "Line: 1, Col: 1, Idx: 0 - Line: 1, Col: 38, Idx: 37"),
-                ("Unexpected Parameter 'Term' in 'Print'", "Line: 1, Col: 1, Idx: 0 - Line: 1, Col: 38, Idx: 37"),
-                ("Missing Parameter 'Value' in 'Print'", "Line: 1, Col: 1, Idx: 0 - Line: 1, Col: 38, Idx: 37"));
-
-
-                yield return new DeserializationErrorCase("Compare(Left = Print(Foo = 1), Right = Print(Foo = 2), Operator = Print(Foo = 2))",
-                    ("Unexpected Parameter 'Foo' in 'Print'", "Line: 1, Col: 1, Idx: 0 - Line: 1, Col: 82, Idx: 81"),
-                ("Missing Parameter 'Value' in 'Print'", "Line: 1, Col: 1, Idx: 0 - Line: 1, Col: 82, Idx: 81"),
-                ("Unexpected Parameter 'Foo' in 'Print'", "Line: 1, Col: 1, Idx: 0 - Line: 1, Col: 82, Idx: 81"),
-                ("Missing Parameter 'Value' in 'Print'", "Line: 1, Col: 1, Idx: 0 - Line: 1, Col: 82, Idx: 81"),
-                ("Unexpected Parameter 'Foo' in 'Print'", "Line: 1, Col: 1, Idx: 0 - Line: 1, Col: 82, Idx: 81"),
-                ("Missing Parameter 'Value' in 'Print'", "Line: 1, Col: 1, Idx: 0 - Line: 1, Col: 82, Idx: 81")
-                );
-
-
-
-                yield return new DeserializationErrorCase("- <ArrayVar1> = Array(Elements = ['abc', '123'])\n- <ArrayVar2> = Array(Elements = ['abc', '123'])\n- Print(Value = (<ArrayVar1> == <ArrayVar2>))",
-                    ("Cannot compare objects of type 'ListOfString'", "<ArrayVar1> == <ArrayVar2>"));
-
-                yield return new DeserializationErrorCase("MyMegaFunction(Value = true)", ("The step 'MyMegaFunction' does not exist", "Line: 1, Col: 1, Idx: 0 - Line: 1, Col: 29, Idx: 28"));
-
-                yield return new DeserializationErrorCase("- >\n  ForEach(\n    Array = ['a','b','c'],\n    Variable = <char>,\n    Action = Print(Value = <char>))",
-                    ("'ForEach( Array = ['a','b','c'], Variable = <char>, Action = Print(Value = <char>))' is a 'String' but it should be a 'Unit' to be a member of 'Sequence'", "Sequence")
+                yield return new DeserializationErrorCase("ForEach ['a','b','c'] <char> (Print <char>)",
+                    ("'ForEach( Array = ['a','b','c'], Variable = <char>, Action = Print(Value = <char>))' is a 'String' but it should be a 'Unit' to be a member of 'Sequence'",
+                        "ForEach ['a','b','c'] <char> (Print <char>)")
                 );
 
             }
