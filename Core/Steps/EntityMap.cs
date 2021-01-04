68d83fdc677f6b2e89ecb4f64bd0b2bfaa6b9a40
Mark Wainwright <wainwrightml@gmail.com>
2021-01-04 18:14:04 +0000

    Allowed custom variable name in EntityMap

--- Core/Steps/EntityMap.cs
+++ Core/Steps/EntityMap.cs
@@ -13,20 +13,20 @@ namespace Reductech.EDR.Core.Steps
     /// <summary>
     /// Apply a function to every entity in an entity stream.
     /// </summary>
-    public sealed class EntityMap : CompoundStep<Core.Array<Entity>>
+    public sealed class EntityMap : CompoundStep<Array<Entity>>
     {
         /// <inheritdoc />
-        public override async Task<Result<Core.Array<Entity>, IError>> Run(IStateMonad stateMonad, CancellationToken cancellationToken)
+        public override async Task<Result<Array<Entity>, IError>> Run(IStateMonad stateMonad, CancellationToken cancellationToken)
         {
             var entityStreamResult = await EntityStream.Run(stateMonad, cancellationToken);
-            if (entityStreamResult.IsFailure) return entityStreamResult.ConvertFailure<Core.Array<Entity>>();
+            if (entityStreamResult.IsFailure) return entityStreamResult.ConvertFailure<Array<Entity>>();
 
             var currentState = stateMonad.GetState().ToImmutableDictionary();
 
             async ValueTask<Entity> Action(Entity record)
             {
                 using var scopedMonad = new ScopedStateMonad(stateMonad, currentState,
-                    new KeyValuePair<VariableName, object>(VariableName.Entity, record));
+                    new KeyValuePair<VariableName, object>(Variable, record));
 
                 var result = await Function.Run(scopedMonad, cancellationToken);
 
@@ -46,14 +46,29 @@ namespace Reductech.EDR.Core.Steps
         /// </summary>
         [StepProperty(1)]
         [Required]
-        public IStep<Core.Array<Entity>> EntityStream { get; set; } = null!;
+        public IStep<Array<Entity>> EntityStream { get; set; } = null!;
 
         /// <summary>
         /// A function to get the mapped entity, using the variable &lt;Entity&gt;
         /// </summary>
         [StepProperty(2)]
         [Required]
+        [ScopedFunction]
         public IStep<Entity> Function { get; set; } = null!;
+
+        /// <summary>
+        /// The variable name to use in the function.
+        /// </summary>
+        [VariableName(3)]
+        [DefaultValueExplanation("<Entity>")]
+        public VariableName Variable { get; set; } = VariableName.Entity;
+
+        /// <inheritdoc />
+        public override Result<StepContext, IError> TryGetScopedContext(StepContext baseContext, IFreezableStep scopedStep)
+        {
+            return baseContext.TryCloneWithScopedStep(Variable, new ActualTypeReference(typeof(Entity)), scopedStep,
+                new StepErrorLocation(this));
+        }
 
         /// <inheritdoc />
         public override IStepFactory StepFactory => EntityMapStepFactory.Instance;
@@ -62,13 +77,13 @@ namespace Reductech.EDR.Core.Steps
     /// <summary>
     /// Apply a function to every entity in an entity stream.
     /// </summary>
-    public sealed class EntityMapStepFactory : SimpleStepFactory<EntityMap, Core.Array<Entity>>
+    public sealed class EntityMapStepFactory : SimpleStepFactory<EntityMap, Array<Entity>>
     {
         private EntityMapStepFactory() {}
 
         /// <summary>
         /// The instance.
         /// </summary>
-        public static SimpleStepFactory<EntityMap, Core.Array<Entity>> Instance { get; } = new EntityMapStepFactory();
+        public static SimpleStepFactory<EntityMap, Array<Entity>> Instance { get; } = new EntityMapStepFactory();
     }
 }
