266919771c8aec9e10f6b514c3e7d8fac81f74ec
Mark Wainwright <wainwrightml@gmail.com>
2021-01-07 15:53:47 +0000

    Moved logging to multiple files

---
+++ Core/Internal/Logging/LogSituationCore.cs
@@ -0,0 +1,44 @@
+﻿namespace Reductech.EDR.Core.Internal.Logging
+{
+
+/// <summary>
+/// Different LoggingSituations
+/// </summary>
+public enum LogSituationCore
+{
+    /// <summary>
+    /// Whenever a step is entered.
+    /// </summary>
+    EnterStep,
+    /// <summary>
+    /// Whenever a step is exited after success.
+    /// </summary>
+    ExitStepSuccess,
+    /// <summary>
+    /// Whenever a step is existed after failure.
+    /// </summary>
+    ExitStepFailure,
+
+    /// <summary>
+    /// When a path is not fully qualified.
+    /// </summary>
+    QualifyingPath,
+    /// <summary>
+    /// When Path.Combine is given an empty list of paths.
+    /// </summary>
+    NoPathProvided,
+    /// <summary>
+    /// Directory Deleted
+    /// </summary>
+    DirectoryDeleted,
+    /// <summary>
+    /// File Deleted
+    /// </summary>
+    FileDeleted,
+    /// <summary>
+    /// Item to delete did not exist
+    /// </summary>
+    ItemToDeleteDidNotExist
+}
+
+}
