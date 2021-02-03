3044086651e5974519364b7017d3f38086d29520
Mark Wainwright <wainwrightml@gmail.com>
2021-02-03 11:36:42 +0000

    Added RegexMatch tests

---
+++ Core.Tests/Steps/RegexMatchTests.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using Reductech.EDR.Core.Steps;
+using Reductech.EDR.Core.TestHarness;
+using static Reductech.EDR.Core.TestHarness.StaticHelpers;
+
+namespace Reductech.EDR.Core.Tests.Steps
+{
+
+public partial class RegexMatchTests : StepTestBase<RegexMatch, bool>
+{
+    /// <inheritdoc />
+    protected override IEnumerable<StepCase> StepCases
+    {
+        get
+        {
+            yield return new StepCase(
+                "Simple true match",
+                new RegexMatch()
+                {
+                    String     = Constant("hello"),
+                    Pattern    = Constant(@"h\w+"),
+                    IgnoreCase = Constant(false)
+                },
+                true
+            );
+
+            yield return new StepCase(
+                "Simple false match",
+                new RegexMatch()
+                {
+                    String     = Constant("hello"),
+                    Pattern    = Constant(@"H\w+"),
+                    IgnoreCase = Constant(false)
+                },
+                false
+            );
+
+            yield return new StepCase(
+                "Ignore case true match",
+                new RegexMatch()
+                {
+                    String     = Constant("hello"),
+                    Pattern    = Constant(@"H\w+"),
+                    IgnoreCase = Constant(true)
+                },
+                true
+            );
+        }
+    }
+}
+
+}
