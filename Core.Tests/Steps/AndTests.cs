3be26a194c78e426f085fe00e12c70466a57195e
Mark Wainwright <wainwrightml@gmail.com>
2021-02-03 14:54:01 +0000

    Move some tests to their own files

---
+++ Core.Tests/Steps/AndTests.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using Reductech.EDR.Core.Internal;
+using Reductech.EDR.Core.Steps;
+using Reductech.EDR.Core.TestHarness;
+
+namespace Reductech.EDR.Core.Tests.Steps
+{
+
+public partial class AndTests : StepTestBase<And, bool>
+{
+    /// <inheritdoc />
+    protected override IEnumerable<StepCase> StepCases
+    {
+        get
+        {
+            yield return new StepCase(
+                "No terms",
+                new And() { Terms = new ArrayNew<bool>() { Elements = new List<IStep<bool>>() } },
+                true
+            );
+
+            yield return new StepCase(
+                "just true",
+                new And() { Terms = StaticHelpers.Array(true) },
+                true
+            );
+
+            yield return new StepCase(
+                "just false",
+                new And() { Terms = StaticHelpers.Array(false) },
+                false
+            );
+
+            yield return new StepCase(
+                "three trues",
+                new And() { Terms = StaticHelpers.Array(true, true, true) },
+                true
+            );
+        }
+    }
+}
+
+}
---
+++ Core.Tests/Steps/OrTests.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using Reductech.EDR.Core.Internal;
+using Reductech.EDR.Core.Steps;
+using Reductech.EDR.Core.TestHarness;
+
+namespace Reductech.EDR.Core.Tests.Steps
+{
+
+public partial class OrTests : StepTestBase<Or, bool>
+{
+    /// <inheritdoc />
+    protected override IEnumerable<StepCase> StepCases
+    {
+        get
+        {
+            yield return new StepCase(
+                "No terms",
+                new Or() { Terms = new ArrayNew<bool>() { Elements = new List<IStep<bool>>() } },
+                false
+            );
+
+            yield return new StepCase(
+                "just true",
+                new Or() { Terms = StaticHelpers.Array(true) },
+                true
+            );
+
+            yield return new StepCase(
+                "just false",
+                new Or() { Terms = StaticHelpers.Array(false) },
+                false
+            );
+
+            yield return new StepCase(
+                "three falses",
+                new Or() { Terms = StaticHelpers.Array(false, false, false) },
+                false
+            );
+        }
+    }
+}
+
+}
