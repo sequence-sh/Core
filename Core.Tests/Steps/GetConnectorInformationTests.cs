edfc4eaaa9de1c7bf1a15c12fea251ca6d7dd0e4
Mark Wainwright <wainwrightml@gmail.com>
2021-03-11 13:34:44 +0000

    Added GetConnectorInformation

---
+++ Core.Tests/Steps/GetConnectorInformationTests.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using Reductech.EDR.Core.Steps;
+using Reductech.EDR.Core.TestHarness;
+
+namespace Reductech.EDR.Core.Tests.Steps
+{
+
+public partial class
+    GetConnectorInformationTests : StepTestBase<GetConnectorInformation, Array<Entity>>
+{
+    /// <inheritdoc />
+    protected override IEnumerable<StepCase> StepCases
+    {
+        get
+        {
+            var coreEntity = Entity.Create(("Name", "Reductech.EDR.Core"), ("Version", "0.6.0"));
+
+            yield return new StepCase(
+                "Core",
+                new GetConnectorInformation(),
+                new Array<Entity>(new[] { coreEntity })
+            );
+        }
+    }
+}
+
+}
---
+++ Core/Steps/GetConnectorInformation.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using CSharpFunctionalExtensions;
+using Reductech.EDR.Core.Internal;
+using Reductech.EDR.Core.Internal.Errors;
+
+namespace Reductech.EDR.Core.Steps
+{
+
+/// <summary>
+/// Gets information about connectors
+/// </summary>
+public sealed class GetConnectorInformation : CompoundStep<Array<Entity>>
+{
+    /// <inheritdoc />
+    protected override async Task<Result<Array<Entity>, IError>> Run(
+        IStateMonad stateMonad,
+        CancellationToken cancellationToken)
+    {
+        await Task.CompletedTask;
+        var entities = new List<Entity>();
+
+        foreach (var connectorInformation in stateMonad.StepFactoryStore.ConnectorInformations)
+        {
+            var entity = Entity.Create(
+                (nameof(ConnectorInformation.Name), connectorInformation.Name),
+                (nameof(ConnectorInformation.Version), connectorInformation.Version)
+            );
+
+            entities.Add(entity);
+        }
+
+        return new Array<Entity>(entities);
+    }
+
+    /// <inheritdoc />
+    public override IStepFactory StepFactory { get; } =
+        new SimpleStepFactory<GetConnectorInformation, Array<Entity>>();
+}
+
+}
