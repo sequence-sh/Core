1a3755eff1c8c883f2fc8e11fb7a6a8cb1b7097e
Mark Wainwright <wainwrightml@gmail.com>
2021-03-23 11:11:23 +0000

    Added GetApplicationVersion step

---
+++ Core.Tests/Steps/GetApplicationVersionTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using Reductech.EDR.Core.Internal;
+using Reductech.EDR.Core.Steps;
+using Reductech.EDR.Core.TestHarness;
+
+namespace Reductech.EDR.Core.Tests.Steps
+{
+
+public partial class
+    GetApplicationVersionTests : StepTestBase<GetApplicationVersion, StringStream>
+{
+    /// <inheritdoc />
+    protected override IEnumerable<StepCase> StepCases
+    {
+        get
+        {
+            var expectedOutput =
+                ConnectorInformation.Create(Assembly.GetEntryAssembly()!).ToString();
+
+            //This will be the the ReSharper Test Runner
+
+            yield return new StepCase(
+                "Get Application Version",
+                new GetApplicationVersion(),
+                expectedOutput
+            );
+        }
+    }
+}
+
+}
---
+++ Core/Steps/GetApplicationVersion.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using CSharpFunctionalExtensions;
+using Reductech.EDR.Core.Internal;
+using Reductech.EDR.Core.Internal.Errors;
+
+namespace Reductech.EDR.Core.Steps
+{
+
+/// <summary>
+/// Gets the current version of the application
+/// </summary>
+public sealed class GetApplicationVersion : CompoundStep<StringStream>
+{
+    /// <inheritdoc />
+    protected override async Task<Result<StringStream, IError>> Run(
+        IStateMonad stateMonad,
+        CancellationToken cancellationToken)
+    {
+        await Task.CompletedTask;
+
+        var entryAssembly = Assembly.GetEntryAssembly()!;
+
+        var ci = ConnectorInformation.Create(entryAssembly);
+        return new StringStream(ci.ToString());
+    }
+
+    /// <inheritdoc />
+    public override IStepFactory StepFactory { get; } =
+        new SimpleStepFactory<GetApplicationVersion, StringStream>();
+}
+
+}
