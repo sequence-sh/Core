bb1dbe6e18abfdfc794de92f0619cff1230f411e
Mark Wainwright <wainwrightml@gmail.com>
2021-03-25 16:16:28 +0000

    Added Delay step

---
+++ Core/Steps/Delay.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Threading;
+using System.Threading.Tasks;
+using CSharpFunctionalExtensions;
+using Reductech.EDR.Core.Attributes;
+using Reductech.EDR.Core.Internal;
+using Reductech.EDR.Core.Internal.Errors;
+using Reductech.EDR.Core.Util;
+
+namespace Reductech.EDR.Core.Steps
+{
+
+/// <summary>
+/// Delay for a specified amount of time
+/// </summary>
+public sealed class Delay : CompoundStep<Unit>
+{
+    /// <inheritdoc />
+    protected override async Task<Result<Unit, IError>> Run(
+        IStateMonad stateMonad,
+        CancellationToken cancellationToken)
+    {
+        var ms = await Milliseconds.Run(stateMonad, cancellationToken);
+
+        if (ms.IsFailure)
+            return ms.ConvertFailure<Unit>();
+
+        await Task.Delay(ms.Value, cancellationToken);
+
+        return Unit.Default;
+    }
+
+    /// <summary>
+    /// The number of milliseconds to delay
+    /// </summary>
+    [StepProperty(1)]
+    [Required]
+    public IStep<int> Milliseconds { get; set; } = null!;
+
+    /// <inheritdoc />
+    public override IStepFactory StepFactory { get; } = new SimpleStepFactory<Delay, Unit>();
+}
+
+}
