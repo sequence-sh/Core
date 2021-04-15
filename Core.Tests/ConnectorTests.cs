645d7187627236ef0c91c989ca1e05f7fc56766a
Mark Wainwright <wainwrightml@gmail.com>
2021-04-15 16:39:40 +0100

    Added PluginLoadContext

---
+++ Core.Tests/ConnectorTests.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Divergic.Logging.Xunit;
+using Reductech.EDR.Core.Abstractions;
+using Reductech.EDR.Core.Connectors;
+using Reductech.EDR.Core.Internal;
+using Reductech.EDR.Core.Internal.Serialization;
+using Reductech.EDR.Core.TestHarness;
+using Xunit;
+
+namespace Reductech.EDR.Core.Tests
+{
+
+[AutoTheory.UseTestOutputHelper]
+public partial class ConnectorTests
+{
+    [Fact]
+    public async Task TestConnector()
+    {
+        var logger = new TestOutputLogger("Step Logger", TestOutputHelper);
+
+        var relativePath = @"ExampleConnector\bin\Debug\net5.0\ExampleConnector.dll";
+        var absolutePath = PluginLoadContext.GetAbsolutePath(relativePath);
+
+        var assembly = PluginLoadContext.LoadPlugin(
+            absolutePath,
+            logger
+        );
+
+        assembly.ShouldBeSuccessful(x => x.AsString);
+
+        var stepTypes = assembly.Value.GetTypes()
+            .Where(x => typeof(IStep).IsAssignableFrom(x))
+            .ToList();
+
+        foreach (var type in stepTypes)
+        {
+            TestOutputHelper.WriteLine(type.Name);
+        }
+
+        var stepFactoryStore = StepFactoryStore.CreateUsingReflection(stepTypes.First());
+
+        var runner = new SCLRunner(
+            SCLSettings.EmptySettings,
+            logger,
+            stepFactoryStore,
+            ExternalContext.Default
+        );
+
+        var r = await
+            runner.RunSequenceFromTextAsync(
+                "Log (GetTestString)",
+                new Dictionary<string, object>(),
+                CancellationToken.None
+            );
+
+        r.ShouldBeSuccessful(x => x.AsString);
+    }
+}
+
+}
---
+++ ExampleConnector/GetTestString.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using CSharpFunctionalExtensions;
+using Reductech.EDR.Core;
+using Reductech.EDR.Core.Internal;
+using Reductech.EDR.Core.Internal.Errors;
+
+namespace ExampleConnector
+{
+
+public sealed class GetTestString : CompoundStep<StringStream>
+{
+    /// <inheritdoc />
+    protected override async Task<Result<StringStream, IError>> Run(
+        IStateMonad stateMonad,
+        CancellationToken cancellationToken)
+    {
+        await Task.CompletedTask;
+
+        return new StringStream(TestString);
+    }
+
+    public const string TestString = "The Ultimate Test String";
+
+    /// <inheritdoc />
+    public override IStepFactory StepFactory { get; } =
+        new SimpleStepFactory<GetTestString, StringStream>();
+}
+
+}
