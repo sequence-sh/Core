ea390e9ebe67526b6e3c176b93288b07e230b9b8
Mark Wainwright <wainwrightml@gmail.com>
2021-04-15 12:47:56 +0100

    SpaceCompressor now compresses escaped newlines

--- Core.TestHarness/SpaceCompressor.cs
+++ Core.TestHarness/SpaceCompressor.cs
@@ -8,11 +8,16 @@ public static class SpaceCompressor
     /// <summary>
     /// Regex for blank spaces
     /// </summary>
-    private static readonly Regex SpaceRegex = new(@"(?:\s+|\r\n|\n)", RegexOptions.Compiled);
+    private static readonly Regex SpaceRegex =
+        new(@"(?:\s+|\r\n|\n|\t|\\r\\n|\\n|\\t)", RegexOptions.Compiled);
 
     private static readonly Regex NewLineRegex = new(@"(?:\r\n|\n)", RegexOptions.Compiled);
 
+    /// <summary>
+    /// Compresses whitespace characters AND escaped whitespace characters
+    /// </summary>
     public static string CompressSpaces(string text) => SpaceRegex.Replace(text, " ");
+
     public static string CompressNewLines(string text) => NewLineRegex.Replace(text, " ");
 }
 
