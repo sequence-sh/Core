7cc660ef0e33ae5c347c74a6165c3e7bec41e19a
Mark Wainwright <wainwrightml@gmail.com>
2021-05-26 13:17:35 +0100

    Added ExternalProcess error tests

--- Core.Tests/ExternalProcessTests.cs
+++ Core.Tests/ExternalProcessTests.cs
@@ -1,10 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
-using Divergic.Logging.Xunit;
 using FluentAssertions;
 using Microsoft.Extensions.Logging;
 using Moq;
@@ -52,6 +51,88 @@ public partial class ExternalProcessTests
         var messages = loggerFactory.Sink.LogEntries.Select(x => x.Message).ToList();
 
         messages.Should().BeEquivalentTo("hello");
+    }
+
+    [Fact]
+    public async Task RunExternalProcess_MustReturnErrors()
+    {
+        var repo = new MockRepository(MockBehavior.Strict);
+
+        var loggerFactory =
+            MELT.TestLoggerFactory.Create(x => x.FilterByMinimumLevel(LogLevel.Information));
+
+        var stateMonad = new StateMonad(
+            loggerFactory.CreateLogger("ExternalProcess"),
+            SCLSettings.EmptySettings,
+            StepFactoryStore.Create(),
+            repo.OneOf<IExternalContext>(),
+            new Dictionary<string, object>()
+        );
+
+        var result = await ExternalProcessRunner.Instance.RunExternalProcess(
+            "pwsh",
+            IgnoreNoneErrorHandler.Instance,
+            new List<string>() { "--command", "write-error 'test error'" },
+            new Dictionary<string, string>(),
+            Encoding.Default,
+            stateMonad,
+            null,
+            new CancellationToken()
+        );
+
+        result.ShouldBeFailure();
+        result.Error.AsString.Should().Contain("test error");
+    }
+
+    [Fact]
+    public async Task RunExternalProcess_ShouldIgnoreErrorsCorrectly()
+    {
+        var repo = new MockRepository(MockBehavior.Strict);
+
+        var loggerFactory =
+            MELT.TestLoggerFactory.Create(x => x.FilterByMinimumLevel(LogLevel.Information));
+
+        var stateMonad = new StateMonad(
+            loggerFactory.CreateLogger("ExternalProcess"),
+            SCLSettings.EmptySettings,
+            StepFactoryStore.Create(),
+            repo.OneOf<IExternalContext>(),
+            new Dictionary<string, object>()
+        );
+
+        var result = await ExternalProcessRunner.Instance.RunExternalProcess(
+            "pwsh",
+            new SelectiveErrorHandler(".*test error.*"),
+            new List<string>() { "--command", "write-error 'test error'; write-output 'hello'" },
+            new Dictionary<string, string>(),
+            Encoding.Default,
+            stateMonad,
+            null,
+            new CancellationToken()
+        );
+
+        result.ShouldBeSuccessful();
+
+        var messages = loggerFactory.Sink.LogEntries.Select(x => x.Message).ToList();
+
+        messages.Should().BeEquivalentTo("[91mWrite-Error: [91mtest error[0m", "hello");
+    }
+
+    private class SelectiveErrorHandler : IErrorHandler
+    {
+        public readonly string IgnoreRegex;
+
+        public SelectiveErrorHandler(string regex)
+        {
+            IgnoreRegex = regex;
+        }
+
+        /// <inheritdoc />
+        public bool ShouldIgnoreError(string s)
+        {
+            var r = Regex.IsMatch(s, IgnoreRegex);
+            return r;
+        }
     }
 }
 
