df9b8b8e52bced7fc50da159fba746cbbc4e91f3
Mark Wainwright <wainwrightml@gmail.com>
2021-05-26 13:30:34 +0100

    Added more RunProcess tests

--- Core.Tests/ExternalProcessTests.cs
+++ Core.Tests/ExternalProcessTests.cs
@@ -10,6 +10,7 @@ using Moq;
 using Reductech.EDR.Core.Abstractions;
 using Reductech.EDR.Core.ExternalProcesses;
 using Reductech.EDR.Core.Internal;
+using Reductech.EDR.Core.Internal.Errors;
 using Reductech.EDR.Core.TestHarness;
 using Xunit;
 
@@ -19,21 +20,52 @@ namespace Reductech.EDR.Core.Tests
 [AutoTheory.UseTestOutputHelper]
 public partial class ExternalProcessTests
 {
-    [Fact]
-    public async Task RunExternalProcess_MustRunProcess()
+    public static StateMonad CreateStateMonad(ILogger logger)
     {
         var repo = new MockRepository(MockBehavior.Strict);
 
-        var loggerFactory =
-            MELT.TestLoggerFactory.Create(x => x.FilterByMinimumLevel(LogLevel.Information));
-
         var stateMonad = new StateMonad(
-            loggerFactory.CreateLogger("ExternalProcess"),
+            logger,
             SCLSettings.EmptySettings,
             StepFactoryStore.Create(),
             repo.OneOf<IExternalContext>(),
             new Dictionary<string, object>()
         );
+
+        return stateMonad;
+    }
+
+    [Fact]
+    public async Task RunExternalProcess_ShouldErrorIfProcessIsMissing()
+    {
+        var loggerFactory =
+            MELT.TestLoggerFactory.Create(x => x.FilterByMinimumLevel(LogLevel.Information));
+
+        var stateMonad = CreateStateMonad(loggerFactory.CreateLogger("ExternalProcess"));
+
+        var result = await ExternalProcessRunner.Instance.RunExternalProcess(
+            "MyRidiculousImpossibleProcess",
+            IgnoreNoneErrorHandler.Instance,
+            new List<string>() { "--command", "write-output 'hello'" },
+            new Dictionary<string, string>(),
+            Encoding.Default,
+            stateMonad,
+            null,
+            new CancellationToken()
+        );
+
+        result.ShouldBeFailure(
+            ErrorCode.ExternalProcessNotFound.ToErrorBuilder("MyRidiculousImpossibleProcess")
+        );
+    }
+
+    [Fact]
+    public async Task RunExternalProcess_MustRunProcess()
+    {
+        var loggerFactory =
+            MELT.TestLoggerFactory.Create(x => x.FilterByMinimumLevel(LogLevel.Information));
+
+        var stateMonad = CreateStateMonad(loggerFactory.CreateLogger("ExternalProcess"));
 
         var result = await ExternalProcessRunner.Instance.RunExternalProcess(
             "pwsh",
@@ -54,20 +86,38 @@ public partial class ExternalProcessTests
     }
 
     [Fact]
-    public async Task RunExternalProcess_MustReturnErrors()
+    public async Task RunExternalProcess_MustPassEnvironmentVariables()
     {
-        var repo = new MockRepository(MockBehavior.Strict);
-
         var loggerFactory =
             MELT.TestLoggerFactory.Create(x => x.FilterByMinimumLevel(LogLevel.Information));
 
-        var stateMonad = new StateMonad(
-            loggerFactory.CreateLogger("ExternalProcess"),
-            SCLSettings.EmptySettings,
-            StepFactoryStore.Create(),
-            repo.OneOf<IExternalContext>(),
-            new Dictionary<string, object>()
+        var stateMonad = CreateStateMonad(loggerFactory.CreateLogger("ExternalProcess"));
+
+        var result = await ExternalProcessRunner.Instance.RunExternalProcess(
+            "pwsh",
+            IgnoreNoneErrorHandler.Instance,
+            new List<string>() { "--command", "write-output $env:TestVariable" },
+            new Dictionary<string, string>() { { "TestVariable", "hello" } },
+            Encoding.Default,
+            stateMonad,
+            null,
+            new CancellationToken()
         );
+
+        result.ShouldBeSuccessful();
+
+        var messages = loggerFactory.Sink.LogEntries.Select(x => x.Message).ToList();
+
+        messages.Should().BeEquivalentTo("hello");
+    }
+
+    [Fact]
+    public async Task RunExternalProcess_MustReturnErrors()
+    {
+        var loggerFactory =
+            MELT.TestLoggerFactory.Create(x => x.FilterByMinimumLevel(LogLevel.Information));
+
+        var stateMonad = CreateStateMonad(loggerFactory.CreateLogger("ExternalProcess"));
 
         var result = await ExternalProcessRunner.Instance.RunExternalProcess(
             "pwsh",
@@ -87,22 +137,14 @@ public partial class ExternalProcessTests
     [Fact]
     public async Task RunExternalProcess_ShouldIgnoreErrorsCorrectly()
     {
-        var repo = new MockRepository(MockBehavior.Strict);
-
         var loggerFactory =
             MELT.TestLoggerFactory.Create(x => x.FilterByMinimumLevel(LogLevel.Information));
 
-        var stateMonad = new StateMonad(
-            loggerFactory.CreateLogger("ExternalProcess"),
-            SCLSettings.EmptySettings,
-            StepFactoryStore.Create(),
-            repo.OneOf<IExternalContext>(),
-            new Dictionary<string, object>()
-        );
+        var stateMonad = CreateStateMonad(loggerFactory.CreateLogger("ExternalProcess"));
 
         var result = await ExternalProcessRunner.Instance.RunExternalProcess(
             "pwsh",
-            new SelectiveErrorHandler(".*test error.*"),
+            IgnoreAllErrorHandler.Instance,
             new List<string>() { "--command", "write-error 'test error'; write-output 'hello'" },
             new Dictionary<string, string>(),
             Encoding.Default,
@@ -116,6 +158,32 @@ public partial class ExternalProcessTests
         var messages = loggerFactory.Sink.LogEntries.Select(x => x.Message).ToList();
 
         messages.Should().BeEquivalentTo("[91mWrite-Error: [91mtest error[0m", "hello");
+    }
+
+    [Fact]
+    public async Task RunExternalProcess_ShouldIgnoreErrorsCorrectly2()
+    {
+        var loggerFactory =
+            MELT.TestLoggerFactory.Create(x => x.FilterByMinimumLevel(LogLevel.Information));
+
+        var stateMonad = CreateStateMonad(loggerFactory.CreateLogger("ExternalProcess"));
+
+        var result = await ExternalProcessRunner.Instance.RunExternalProcess(
+            "pwsh",
+            new SelectiveErrorHandler(".*ignore this error.*"),
+            new List<string>()
+            {
+                "--command", "write-error 'ignore this error'; write-error 'test error'"
+            },
+            new Dictionary<string, string>(),
+            Encoding.Default,
+            stateMonad,
+            null,
+            new CancellationToken()
+        );
+
+        result.ShouldBeFailure();
+        result.Error.AsString.Should().Contain("test error");
     }
 
     private class SelectiveErrorHandler : IErrorHandler
