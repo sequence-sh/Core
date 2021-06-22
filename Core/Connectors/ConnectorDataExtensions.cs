b9df277dba949b42ab9d984d31246a27b66e177f
antonyoni <antony@reduc.tech>
2021-06-22 19:46:10 +0100

    Add extension methods for ConnectorManager classes These are all the methods that need core components

---
+++ Core/Connectors/ConnectorDataExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CSharpFunctionalExtensions;
+using Reductech.EDR.ConnectorManagement;
+using Reductech.EDR.Core.Internal.Errors;
+
+namespace Reductech.EDR.Core.Connectors
+{
+
+/// <summary>
+/// Provides Core-specific extension methods for ConnectorData
+/// </summary>
+public static class ConnectorDataExtensions
+{
+    /// <summary>
+    /// Get injections for this connector
+    /// </summary>
+    public static IReadOnlyCollection<IConnectorInjection> GetConnectorInjections(
+        this ConnectorData connectorData) => connectorData.Assembly?.GetTypes()
+        .Where(x => !x.IsAbstract)
+        .Where(x => typeof(IConnectorInjection).IsAssignableFrom(x))
+        .Select(Activator.CreateInstance)
+        .Cast<IConnectorInjection>()
+        .ToArray() ?? ArraySegment<IConnectorInjection>.Empty;
+
+    /// <summary>
+    /// Tries to get contexts injected by connectors
+    /// </summary>
+    public static Result<(string Name, object Context)[], IErrorBuilder> TryGetInjectedContexts(
+        this ConnectorData connectorData,
+        SCLSettings settings)
+    {
+        var contexts = connectorData.GetConnectorInjections()
+            .Select(x => x.TryGetInjectedContexts(settings))
+            .Combine(x => x.SelectMany(y => y).ToArray(), ErrorBuilderList.Combine);
+
+        return contexts;
+    }
+}
+
+}
---
+++ Core/Connectors/ConnectorSettingsExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using Reductech.EDR.ConnectorManagement;
+using Reductech.EDR.Core.Entities;
+
+namespace Reductech.EDR.Core.Connectors
+{
+
+/// <summary>
+/// Helpers for converting settings
+/// </summary>
+public static class ConnectorSettingsHelper
+{
+    /// <summary>
+    /// Create a settings from SCLSettings
+    /// </summary>
+    public static IEnumerable<(string Key, ConnectorSettings Settings)> CreateFromSCLSettings(
+        SCLSettings sclSettings)
+    {
+        var connectorsEntity = sclSettings.Entity.TryGetNestedEntity(SCLSettings.ConnectorsKey);
+
+        if (connectorsEntity.HasNoValue)
+            yield break;
+
+        foreach (var property in connectorsEntity.Value)
+        {
+            if (property.BestValue is EntityValue.NestedEntity nestedEntity)
+            {
+                var convertResult =
+                    EntityConversionHelpers.TryCreateFromEntity<ConnectorSettings>(
+                        nestedEntity.Value
+                    );
+
+                if (convertResult.IsSuccess)
+                    yield return new(property.Name, convertResult.Value);
+            }
+        }
+    }
+}
+
+}
