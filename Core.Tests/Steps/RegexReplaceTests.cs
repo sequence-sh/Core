fb859feded2d3f1d1f63f945206cdc6dd2a05050
Mark Wainwright <wainwrightml@gmail.com>
2021-08-25 15:11:00 +0100

    Added Replace parameter to regex replace

--- Core.Tests/Steps/RegexReplaceTests.cs
+++ Core.Tests/Steps/RegexReplaceTests.cs
@@ -1,7 +1,10 @@
 ﻿using System.Collections.Generic;
+using FluentAssertions;
 using Reductech.EDR.Core.Internal;
+using Reductech.EDR.Core.Internal.Errors;
 using Reductech.EDR.Core.Steps;
 using Reductech.EDR.Core.TestHarness;
+using Xunit;
 using static Reductech.EDR.Core.TestHarness.StaticHelpers;
 
 namespace Reductech.EDR.Core.Tests.Steps
@@ -92,6 +95,51 @@ public partial class RegexReplaceTests : StepTestBase<RegexReplace, StringStream
                 "Number _1_, _2_, _3_"
             );
         }
+    }
+
+    /// <inheritdoc />
+    protected override IEnumerable<ErrorCase> ErrorCases
+    {
+        get
+        {
+            foreach (var errorCase in base.ErrorCases)
+            {
+                yield return errorCase;
+            }
+
+            yield return new ErrorCase(
+                "Neither Replace nor Function is set",
+                new RegexReplace() { String = Constant("blah"), Pattern = Constant("Pattern") },
+                ErrorCode.MissingParameter.ToErrorBuilder(nameof(RegexReplace.Replace))
+            );
+        }
+    }
+
+    [Fact]
+    public void ConflictingParametersShouldNotPassVerify()
+    {
+        var step = new RegexReplace()
+        {
+            String  = Constant("blah"),
+            Pattern = Constant("Pattern"),
+            Replace = Constant("Replace"),
+            Function = new LambdaFunction<StringStream, StringStream>(
+                null,
+                Constant("Replacement")
+            )
+        };
+
+        var verifyResult = step.VerifyThis(StepFactoryStore.Create());
+
+        verifyResult.ShouldBeFailure();
+
+        verifyResult.Error.ToErrorBuilder.Should()
+            .Be(
+                ErrorCode.ConflictingParameters.ToErrorBuilder(
+                    nameof(RegexReplace.Replace),
+                    nameof(RegexReplace.Function)
+                )
+            );
     }
 }
 
