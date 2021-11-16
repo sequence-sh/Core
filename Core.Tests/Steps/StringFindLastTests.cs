b897ddf25ed7c9cef03fc6f8385fe071ff1d90ee
antonyoni <antony@reduc.tech>
2021-11-16 15:52:02 +0000

    Rename FindLastSubstring to StringFindLast

---
+++ Core.Tests/Steps/StringFindLastTests.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using Reductech.EDR.Core.Steps;
+using Reductech.EDR.Core.TestHarness;
+using static Reductech.EDR.Core.TestHarness.StaticHelpers;
+
+namespace Reductech.EDR.Core.Tests.Steps
+{
+
+public partial class StringFindLastTests : StepTestBase<StringFindLast, int>
+{
+    /// <inheritdoc />
+    protected override IEnumerable<StepCase> StepCases
+    {
+        get
+        {
+            yield return new StepCase(
+                "Substring present",
+                new StringFindLast()
+                {
+                    String = Constant("Hello elle"), SubString = Constant("ell")
+                },
+                6
+            );
+
+            yield return new StepCase(
+                "Substring not present",
+                new StringFindLast()
+                {
+                    String = Constant("Hello elle"), SubString = Constant("ELL")
+                },
+                -1
+            );
+        }
+    }
+
+    /// <inheritdoc />
+    protected override IEnumerable<DeserializeCase> DeserializeCases
+    {
+        get
+        {
+            yield return new DeserializeCase(
+                "Substring present",
+                "StringFindLast String: 'Hello ell' Substring: 'ell'",
+                6
+            );
+        }
+    }
+}
+
+}
---
+++ Core/Steps/StringFindLast.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Threading;
+using System.Threading.Tasks;
+using CSharpFunctionalExtensions;
+using Reductech.EDR.Core.Attributes;
+using Reductech.EDR.Core.Internal;
+using Reductech.EDR.Core.Internal.Errors;
+
+namespace Reductech.EDR.Core.Steps
+{
+
+/// <summary>
+/// Gets the last instance of substring in a string.
+/// Returns -1 if the substring is not present
+/// </summary>
+[Alias("LastIndexOfSubstring")]
+public sealed class StringFindLast : CompoundStep<int>
+{
+    /// <summary>
+    /// The string to check.
+    /// </summary>
+    [StepProperty(1)]
+    [Required]
+    public IStep<StringStream> String { get; set; } = null!;
+
+    /// <summary>
+    /// The substring to find.
+    /// </summary>
+    [StepProperty(2)]
+    [Required]
+    public IStep<StringStream> SubString { get; set; } = null!;
+
+    /// <inheritdoc />
+    protected override async Task<Result<int, IError>> Run(
+        IStateMonad stateMonad,
+        CancellationToken cancellationToken)
+    {
+        var str = await String.Run(stateMonad, cancellationToken)
+            .Map(async x => await x.GetStringAsync());
+
+        if (str.IsFailure)
+            return str.ConvertFailure<int>();
+
+        var subString = await SubString.Run(stateMonad, cancellationToken)
+            .Map(async x => await x.GetStringAsync());
+
+        if (subString.IsFailure)
+            return subString.ConvertFailure<int>();
+
+        return str.Value.LastIndexOf(subString.Value, StringComparison.Ordinal);
+    }
+
+    /// <inheritdoc />
+    public override IStepFactory StepFactory { get; } =
+        new SimpleStepFactory<StringFindLast, int>();
+}
+
+}
