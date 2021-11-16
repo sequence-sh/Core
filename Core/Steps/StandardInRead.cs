00c084e4888a72f69528374bfe18bb3905542d3f
antonyoni <antony@reduc.tech>
2021-11-16 16:20:49 +0000

    Rename ReadStandardIn to StandardInRead

---
+++ Core/Steps/StandardInRead.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using CSharpFunctionalExtensions;
+using Reductech.EDR.Core.Enums;
+using Reductech.EDR.Core.Internal;
+using Reductech.EDR.Core.Internal.Errors;
+
+namespace Reductech.EDR.Core.Steps
+{
+
+/// <summary>
+/// Reads the Console standard input
+/// </summary>
+public class StandardInRead : CompoundStep<StringStream>
+{
+    /// <inheritdoc />
+    protected override async Task<Result<StringStream, IError>> Run(
+        IStateMonad stateMonad,
+        CancellationToken cancellationToken)
+    {
+        await ValueTask.CompletedTask;
+
+        var ss = new StringStream(
+            stateMonad.ExternalContext.Console.OpenStandardInput(),
+            EncodingEnum.UTF8
+        );
+
+        return ss;
+    }
+
+    /// <inheritdoc />
+    public override IStepFactory StepFactory { get; } =
+        new SimpleStepFactory<StandardInRead, StringStream>();
+}
+
+}
