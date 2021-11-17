c3d768e4207512ec4dee73949e1286bf27f60381
antonyoni <antony@reduc.tech>
2021-11-17 17:05:46 +0000

    Rename from ArrayConcat2Tests to ArrayConcatTests

--- Core.Tests/Steps/ArrayConcatTests.cs
+++ Core.Tests/Steps/ArrayConcatTests.cs
@@ -1,14 +1,11 @@
 ﻿using System.Collections.Generic;
-using Reductech.EDR.Core.Internal;
 using Reductech.EDR.Core.Steps;
 using Reductech.EDR.Core.TestHarness;
-using Reductech.EDR.Core.Util;
-using static Reductech.EDR.Core.TestHarness.StaticHelpers;
 
 namespace Reductech.EDR.Core.Tests.Steps
 {
 
-public partial class ArrayConcatTests2 : StepTestBase<ArrayConcat<int>, Array<int>>
+public partial class ArrayConcatTests : StepTestBase<ArrayConcat<int>, Array<int>>
 {
     /// <inheritdoc />
     protected override IEnumerable<StepCase> StepCases
