c15ebf2b7734dfd2a9334f2a9bff1d725cdcb5b2
Mark Wainwright <wainwrightml@gmail.com>
2022-03-11 14:55:28 +0000

    Added tests for getting parameter values

---
+++ Core.Tests/GetParameterValuesTests.cs
@@ -0,0 +1,39 @@
+﻿using Reductech.Sequence.Core.Internal.Parser;
+
+namespace Reductech.Sequence.Core.Tests;
+
+public class GetParameterValuesTests
+{
+    public GetParameterValuesTests(ITestOutputHelper testOutputHelper)
+    {
+        TestOutputHelper = testOutputHelper;
+    }
+
+    public ITestOutputHelper TestOutputHelper { get; set; }
+
+    [Fact]
+    public void GetParameterValuesShouldReturnCorrectValues()
+    {
+        const string text = $"RunExternalProcess 'myPath' Arguments: ['a', 'b', 'c']";
+        var          sfs  = StepFactoryStore.Create();
+
+        var parseResult = SCLParsing
+            .TryParseStep(text)
+            .Bind(x => x.TryFreeze(SCLRunner.RootCallerMetadata, sfs));
+
+        parseResult.ShouldBeSuccessful();
+
+        var pvs = parseResult.Value.GetParameterValues().ToList();
+
+        var pathParameters = pvs.Where(x => x.Parameter.Metadata.ContainsKey("path")).ToList();
+
+        pathParameters.Should().HaveCount(1);
+
+        pathParameters.Single().Value.TryGetConstantValue().ShouldHaveValue();
+
+        pathParameters.Single()
+            .Value.TryGetConstantValue()
+            .Value.Should()
+            .Be(new StringStream("myPath"));
+    }
+}
