aefca303a240faf4ad732691482b8222a3d3cd6c
Mark Wainwright <wainwrightml@gmail.com>
2022-10-31 11:26:43 +0000

    Added ArrayReverse step and tests

---
+++ Core.Tests/Steps/ArrayReverseTests.cs
@@ -0,0 +1,7 @@
+﻿namespace Reductech.Sequence.Core.Tests.Steps;
+
+public partial class ArrayReverseTests : StepTestBase<ArrayReverse<SCLInt>, Array<SCLInt>>
+{
+    /// <inheritdoc />
+    protected override IEnumerable<StepCase> StepCases { get; } = new List<StepCase>();
+}
